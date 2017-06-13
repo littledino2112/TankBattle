c8110ffefe51fba4af05a4a411fbaf5ae9dd1fdd
Hoang Nguyen <hoangnguyen@misfit.com>
2017-06-13 15:41:53 +0700

    Add function build_node_name as a utility

--- ShortestPath.m
+++ ShortestPath.m
@@ -7,19 +7,28 @@ G = graph; % Create empty graph
 for i=1:row_len
    for j=1:col_len
       if (input_data(i,j) ~= 0)
-          node_name = sprintf('%d_%d',i,j);
+          node_name = build_node_name(i,j,input_data(i,j));
           if (j < col_len)
             if (input_data(i,j+1) ~= 0)
-                node_name_1 = sprintf('%d_%d',i,j+1);
+                node_name_1 = build_node_name(i,j+1,input_data(i,j+1));
                 G = addedge(G,node_name,node_name_1);
             end
           end
           if (i < row_len)
             if (input_data(i+1,j) ~= 0)
-                node_name_2 = sprintf('%d_%d',i+1,j);
+                node_name_2 = build_node_name(i+1,j,input_data(i+1,j));
                 G = addedge(G,node_name,node_name_2);
             end
           end
       end    
    end
+end
+
+function node_name = build_node_name(row, col, value)
+    node_name = sprintf('%d_%d',row,col);
+    if (value == 2)
+        node_name = ['G_' node_name];   % Gas station
+    elseif (value == 3)
+        node_name = ['H_' node_name];   % Heart
+    end
 end
